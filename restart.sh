cbda016e57c254d2252b0325a29f2902457f53ec
Fiaguhop137 <eastonzou@gmail.com>
2026-08-04 16:10:21 -0400

    Auto-commit: 2026-08-04 16:10:21

--- restart.sh
+++ restart.sh
@@ -2,6 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 sleep 1
-pkill -f "python3 emberbot137.py"
+pkill -f "python3 emberbot137.py" || :
 git sync || :
 nohup python3 emberbot137.py > emberbot137.log 2>&1 &

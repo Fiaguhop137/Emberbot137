e831aacf25a902a873ca19c942dd4a653d16c7ae
Fiaguhop137 <eastonzou@gmail.com>
2026-09-07 22:04:58 -0400

    Auto-commit: 2026.09.07.22.04.58

--- restart.sh
+++ restart.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -e
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+export HOME=/home/firebot
+export USER=firebot
+export LOGNAME=firebot
 export XDG_RUNTIME_DIR=/run/user/1000
-cd "$(dirname "$0")"
+
+cd /home/firebot/git/Emberbot137
 sleep 1
-pkill -f "emberbot137.py" || :
-pkill -x "xz" || :
-git sync || :
-nohup python3 /home/firebot/git/Emberbot137/emberbot137.py > /home/firebot/git/Emberbot137/emberbot137.log 2>&1 &
+
+/usr/bin/pkill -f "emberbot137.py" || :
+/usr/bin/pkill -x "xz" || :
+
+/home/firebot/Downloads/shell/git-sync.sh || :
+
+/usr/bin/nohup /usr/bin/python3 /home/firebot/git/Emberbot137/emberbot137.py \
+    > /home/firebot/git/Emberbot137/emberbot137.log 2>&1 &
